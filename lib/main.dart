f7cd8bdd73cdc9930b564b4a883a33dce6b40ace
Y-h57 <si20051@edu.aomori-u.ac.jp>
2023-08-01 12:43:39 +0900

    入力ボタンの作成

--- lib/main.dart
+++ lib/main.dart
@@ -2,32 +2,62 @@ import 'package:flutter/material.dart';
 
 void main() {
   // 最初に表示するWidget
-  runApp(MyTodoApp());
+  runApp(MyApp());
 }
 
-class MyTodoApp extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       // アプリ名
-      title: 'My Todo App',
+      title: 'My Prevent Waste App',
       theme: ThemeData(
         // テーマカラー
         primarySwatch: Colors.blue,
       ),
-      // リスト一覧画面を表示
-      home: TodoListPage(),
+      // 一覧画面を表示
+      home: ListPage(),
     );
   }
 }
 
-// リスト一覧画面用Widget
-class TodoListPage extends StatelessWidget {
+// 一覧画面用Widget
+class ListPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text('リスト一覧画面'),
+        child: ElevatedButton(
+          onPressed: () {
+            // "push"で新規画面に遷移
+            Navigator.of(context).push(
+              MaterialPageRoute(builder: (context) {
+                // 入力・編集画面を指定
+                return GraphPage();
+              }),
+            );
+          },
+          child: Text('入力'),
+        ),
+      ),
+    );
+  }
+}
+
+// 入力・編集画面用Widget
+class GraphPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: TextButton(
+          // ボタンをクリックした時の処理
+          onPressed: () {
+            // "pop"で前の画面に戻る
+            Navigator.of(context).pop();
+          },
+          child: Text('一覧画面（クリックで戻る）'),
+        ),
       ),
     );
   }
