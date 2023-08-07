efdcddff125cd8ef400a92943c22ee995892d26e
Y-h57 <si20051@edu.aomori-u.ac.jp>
2023-08-07 09:48:11 +0900

    画面遷移

--- lib/main.dart
+++ lib/main.dart
@@ -26,25 +26,56 @@ class ListPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            // "push"で新規画面に遷移
-            Navigator.of(context).push(
-              MaterialPageRoute(builder: (context) {
-                // 入力・編集画面を指定
-                return GraphPage();
-              }),
-            );
-          },
-          child: Text('入力'),
-        ),
+      body: Column(
+        children:<Widget>[
+          ElevatedButton(
+            onPressed: (){
+              // "push"で新規画面に遷移
+              Navigator.of(context).push(
+                MaterialPageRoute(builder: (context) {
+                  // 入力・編集画面を指定
+                  return InputPage();
+                }),
+              );
+            },
+            child: Text('入力'),
+          ),
+          OutlinedButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(builder: (context) {
+                  return GraphPage();
+                }),
+              );
+            },
+            child: Text('グラフ表示画面'),
+          ),
+        ],
       ),
     );
   }
 }
 
 // 入力・編集画面用Widget
+class InputPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: TextButton(
+          // ボタンをクリックした時の処理
+          onPressed: () {
+            // "pop"で前の画面に戻る
+            Navigator.of(context).pop();
+          },
+          child: Text('戻る'),
+        ),
+      ),
+    );
+  }
+}
+
+// グラフ表示画面用Widget
 class GraphPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -56,7 +87,7 @@ class GraphPage extends StatelessWidget {
             // "pop"で前の画面に戻る
             Navigator.of(context).pop();
           },
-          child: Text('一覧画面（クリックで戻る）'),
+          child: Text('一覧画面へ'),
         ),
       ),
     );
